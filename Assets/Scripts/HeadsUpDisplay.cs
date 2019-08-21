888a0b328c7f085f2da86eb79c54f8933f287839
CptnFabulous <marcus@mouyat.com>
2019-08-21 21:07:54 +1000

    Added more stuff
    Added an alien that doesn't do anything yet, and a simple HUD script that shows the player's ammo counter. I will quickly add in health systems as well.

---
+++ Assets/Scripts/HeadsUpDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeadsUpDisplay : MonoBehaviour
+{
+    public Text healthCounter;
+    public Text ammoCounter;
+    public Gun equippedWeapon;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ammoCounter.text = equippedWeapon.roundsInMagazine.ToString();
+    }
+}
